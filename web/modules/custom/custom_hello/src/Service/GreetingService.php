9c062b399bf38578cf879d8c152185d01c7cd76e
AlejandroPR23 <perezrestrepoalejandro9@gmail.com>
2025-03-27 20:16:59 -0500

    fix: code sniffer errors

--- web/modules/custom/custom_hello/src/Service/GreetingService.php
+++ web/modules/custom/custom_hello/src/Service/GreetingService.php
@@ -2,13 +2,16 @@
 
 namespace Drupal\custom_hello\Service;
 
+/**
+ * Greetin class Service.
+ */
 class GreetingService {
 
   /**
    * Get greeting function.
    */
   public function getGreeting($name) {
-    // Retorna directamente la cadena sin traducción
+    // Retorna directamente la cadena sin traducción.
     return "¡Hola, $name!";
   }
 
--- web/modules/custom/custom_hello/tests/src/Unit/GreetingServiceTest.php
+++ web/modules/custom/custom_hello/tests/src/Unit/GreetingServiceTest.php
@@ -5,8 +5,14 @@ namespace Drupal\Tests\custom_hello\Unit;
 use Drupal\custom_hello\Service\GreetingService;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Greeting Service Test class.
+ */
 class GreetingServiceTest extends TestCase {
 
+  /**
+   * Test greeting function.
+   */
   public function testGetGreeting() {
     $service = new GreetingService();
     $this->assertEquals('¡Hola, Juan!', $service->getGreeting('Juan'));
